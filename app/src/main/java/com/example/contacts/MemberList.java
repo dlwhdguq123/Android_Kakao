8e56302df0de2f99a95bd892f9ada499a67d45d6
01099473080 <01099473080@naver.com>
2019-04-17 17:13:56 +0900

    2019.04.17

--- app/src/main/java/com/example/contacts/MemberList.java
+++ app/src/main/java/com/example/contacts/MemberList.java
@@ -1,20 +1,24 @@
 package com.example.contacts;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import static com.example.contacts.Index.*;
 import java.util.ArrayList;
 
 public class MemberList extends AppCompatActivity {
@@ -29,13 +33,51 @@ public class MemberList extends AppCompatActivity {
 
 
         memberList.setAdapter(
-              new MemberAdapter(_this, (ArrayList<Index.Member>)new Index.ISupplier() {
-                  @Override
-                  public Object get() {
-                      return query.get();
-                  }
-              }.get())
+                        new MemberAdapter(_this, (ArrayList<Member>)new ISupplier() {
+                            @Override
+                            public Object get() {
+                                return query.get();
+                            }
+                        }.get())
         );
+        memberList.setOnItemClickListener
+                ((AdapterView<?> p, View v, int i, long L)->{
+                    Member m = (Member)memberList.getItemAtPosition(i);
+                    Intent intent = new Intent(_this,MemberDetail.class);
+                    Toast.makeText(_this,m.name,Toast.LENGTH_LONG).show();
+                    intent.putExtra("seq",m.seq+"");
+                    startActivity(intent);
+                });
+        memberList.setOnItemLongClickListener(
+            (AdapterView<?> p, View v, int i, long L)->{
+                Member m = (Member)memberList.getItemAtPosition(i);
+                new AlertDialog.Builder(_this)
+                        .setTitle("DELETE")
+                        .setMessage("정말 삭제할까요?")
+                        .setPositiveButton(
+                                android.R.string.yes,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        //삭제 쿼리 실행
+                                        Toast.makeText(_this,"삭제완료 !",Toast.LENGTH_LONG).show();
+                                        startActivity(new Intent(_this,MemberList.class));
+                                    }
+                                }
+                        )
+                        .setNegativeButton(
+                                android.R.string.no,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Toast.makeText(_this,"삭제취소",Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                        )
+                        .show();
+                return true;
+        });
+
     }
     private class MemberListQuery extends Index.QueryFactory{
         SQLiteOpenHelper helper;
@@ -60,10 +102,10 @@ public class MemberList extends AppCompatActivity {
             Cursor c = this.getDatabase().rawQuery(
                      " SELECT * FROM MEMBER ", null
             );
-            Index.Member m = null;
+            Member m = null;
             if(c != null){
                 while(c.moveToNext()){
-                    m = new Index.Member();
+                    m = new Member();
                     m.seq = Integer.parseInt(c.getString(c.getColumnIndex(Index.MSEQ)));
                     m.name = c.getString(c.getColumnIndex(Index.MNAME));
                     m.pw = c.getString(c.getColumnIndex(Index.MPW));
@@ -121,7 +163,30 @@ public class MemberList extends AppCompatActivity {
             }else{
                 holder = (ViewHolder) v.getTag();
             }
+            ItemPhoto query = new ItemPhoto(_this);
+            query.seq = list.get(i).seq+"";
 
+            holder.photo
+                    .setImageDrawable(
+                            getResources()
+                                    .getDrawable(
+                                            getResources()
+                                                    .getIdentifier(
+                                                            _this.getPackageName()+":drawable/"
+                                                                    +((String)new Index.ISupplier() {
+                                                                @Override
+                                                                public Object get() {
+                                                                    return query.get();
+                                                                }
+                                                            }.get()),
+                                                            null,
+                                                            null),
+                                            _this.getTheme()
+                    )
+            );
+
+            holder.name.setText(list.get(i).name);
+            holder.phone.setText(list.get(i).phone);
             return v;
         }
     }
@@ -129,6 +194,38 @@ public class MemberList extends AppCompatActivity {
         ImageView photo;
         TextView name, phone;
     }
+    private class MemberPhotoQuery extends Index.QueryFactory{
+        SQLiteOpenHelper helper;
+        public MemberPhotoQuery(Context _this) {
+            super(_this);
+            helper = new Index.SQLiteHelper(_this);
+        }
 
-
+        @Override
+        public SQLiteDatabase getDatabase() {
+            return helper.getReadableDatabase();
+        }
+    }
+    private class ItemPhoto extends MemberPhotoQuery{
+        String seq;
+        public ItemPhoto(Context _this) {
+            super(_this);
+        }
+        public String get(){
+            Cursor c = getDatabase()
+                    .rawQuery(String.format(
+                            " SELECT %s FROM %s "
+                                    +" WHERE %s LIKE '%s' "
+                            ,Index.MPHOTO, Index.MEMBERS,
+                            Index.MSEQ, seq), null
+                    );
+            String result = "";
+            if(c != null){
+                if(c.moveToNext()){
+                    result = c.getString(c.getColumnIndex(Index.MPHOTO));
+                }
+            }
+            return result;
+        }
+    }
 }
